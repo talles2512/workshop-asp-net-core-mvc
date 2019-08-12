3596bd45323c9398ce632307730f02e58addae41
Hebert Talles <talles_be@hotmail.com>
2019-08-12 12:48:38 -0300

    Update seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
+using System.Collections.Generic;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -77,6 +79,47 @@ namespace SalesWebMvc.Controllers
             }
 
             return View(obj); //iniciando a View Details jogando o objeto 
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) //Caso a busca dê algum tipo de problema
+            {
+                return NotFound(); //View NotFound
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id) //Caso o id passado seja diferente do Id do Vendedor que deve ser alterado
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -37,6 +38,23 @@ namespace SalesWebMvc.Services
             var obj = _context.Seller.Find(id); //procurando um vendedor pelo id no banco de dados e instanciando ele
             _context.Seller.Remove(obj); //removendo-o do banco
             _context.SaveChanges(); //confirmando a operação
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id)) //Procura uma ocorrência que tenha o mesmo Id, ou seja, verifica o registro a ser alterado existe
+            {
+                throw new NotFoundException("Id not found"); //Lança a exceção referente a não ocorrencia de registro
+            }
+            try
+            {
+                _context.Update(obj); //Atualiza o registro de Vendedor passando como parametro o Vendedor alterado
+                _context.SaveChanges(); //confirmando a operação
+            }
+            catch(DbUpdateConcurrencyException e) //Exceção do nivel de acesso a dados
+            {
+                throw new DbConcurrencyException(e.Message); //Lançando uma exceção para ser tratada no nível de serviços
+            }
         }
     }
 }
